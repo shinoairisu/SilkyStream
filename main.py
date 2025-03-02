bab4ea257dc5cebfbef76ad08001067089ce6acd
shinoairisu <37396293+shinoairisu@users.noreply.github.com>
2025-03-02 21:28:54 +0800

    新增几个特殊功能的Item

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import streamlit as st
 from silkystream.data_vm import DataViewModel as dvm
 from silkystream.enhanced_control import EnhancedControl as ec
 
-class TestModel():
-    def __init__(self):
-        self.data_text:str = ""
-    def watch_data_text(self,old,new):
-        print(f"内容变更")
+# class TestModel():
+#     def __init__(self):
+#         self.data_text:str = ""
+#     def watch_data_text(self,old,new):
+#         print(f"内容变更")
 
 
-ts = dvm.set_datavm("page_1",data_class_name=TestModel) # 返回当前页的数据模型
-st.write("""# 数据模型绑定展示
+# ts = dvm.set_datavm("page_1",data_class_name=TestModel) # 返回当前页的数据模型
+# st.write("""# 数据模型绑定展示
          
-- 修改任何单行或者多行文本框的内容，按下enter或者ctrl+enter，单行和多行文本框内容会同时改变。
+# - 修改任何单行或者多行文本框的内容，按下enter或者ctrl+enter，单行和多行文本框内容会同时改变。
 
-""")
-ec.text_input(label="输入你想输入的",model="data_text",key="test1")
-ec.text_area(label="输入你想输入的",model="data_text",key="test2")
+# """)
+# ec.text_input(label="输入你想输入的",model="data_text",key="test1")
+# ec.text_area(label="输入你想输入的",model="data_text",key="test2")
 
+class a(object):
+    """你好"""
+
+print("哦")
--- silkystream/base_classes.py
+++ silkystream/base_classes.py
@@ -1,88 +1,176 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 import copy
-from typing import Tuple
+from typing import Tuple, Union
 from nanoid import generate
 import streamlit as st
 
+
 class AbstractItem(ABC):
-    def __init__(self,value):
+    def __init__(self, value):
         super().__init__()
         self._value = value
 
     @property
     @abstractmethod
-    def value(self):... # 值获取器
-    
+    def value(self): ...  # 值获取器
+
     @value.setter
     @abstractmethod
-    def value(self):... # 值设置器
+    def value(self,value): ...  # 值设置器
 
     def copy(self):
-        return copy.copy(self) # 构造一个新的对象出来
-    
+        return copy.copy(self)  # 构造一个新的对象出来
+
     def __eq__(self, value):
         """默认使用value判等"""
         return self._value == value
+
+    def __int__(self) -> int:
+        try:
+            result = int(self._value)
+        except Exception:
+            result = 0
+        return result
+
+    def __float__(self) -> float:
+        try:
+            result = float(self._value)
+        except Exception:
+            result = 0.0
+        return result
+
+    def __str__(self) -> str:
+        try:
+            result = str(self._value)
+        except Exception:
+            result = ""
+        return result
     
-    def update(self):... # 用于更新数据，比如进度条，可以清理被注册的进度条
+    def __bool__(self) -> bool:
+        """默认的bool是True"""
+        return True
+
+    def rerun(self): ...  # 用于更新数据，比如进度条，可以清理被注册的进度条
+
 
 class BaseItem(AbstractItem):
     """用于基础类型，比如：int str float bool等"""
-    def __init__(self,value):
+
+    def __init__(self, value):
         super().__init__(value)
-    
+
     @property
     def value(self):
         return self._value
+
     @value.setter
-    def value(self,value):
+    def value(self, value):
         self._value = value
 
-class AIHistory(AbstractItem):
+
+class AIHistoryItem(AbstractItem):
     """用于AI对话历史记录"""
-    def __init__(self, value:list):
+
+    def __init__(self, value: list):
         """
         value:是一个列表,可以是一个空列表，也可以是以前的历史记录
+        value列表是复制品
         """
         if not value:
             super().__init__(value)
         else:
             super().__init__(copy.copy(value))
+
     @property
     def value(self):
         """拿到值就可以修改对应内容"""
         return self._value
-    
+
     @value.setter
-    def value(self,value:Tuple[str,str]):
-        """输入值会变成添加值"""
+    def value(self, value: Tuple[str, str]):
+        """输入值会变成添加值 ("user","你好")"""
         self._value.append(value)
 
     def __eq__(self, value):
         """当想关闭一个值的watch判断时，本函数返回True即可"""
         return True
 
+
 class ProgressItem(AbstractItem):
     """用于进度条"""
-    def __init__(self,value,min_value=0,max_value=100):
-        assert min_value <= value <= max_value,"value值超出进度条范围"
-        super().__init__(value)
+
+    def __init__(self, value: Tuple[int | float, str] | int | float, min_value=0, max_value=100):
+        """
+        方案1：(15,"等待中...")  同时放入数字和文字
+        方案2：15 只放入数字
+        """
         self.min_value = min_value
         self.max_value = max_value
-        self.key = generate(size=10)
-        self.regist = [] # 存储注册的进度条
+        self.value_setter(value=value)
+        self.regist = []  # 存储注册的进度条
+        self.validate_value(value=value)
+
+    def validate_value(self,value):
+        assert self.min_value <= value <= self.max_value, "value值超出进度条范围"
     @property
     def value(self):
         return self._value
+
     @value.setter
-    def value(self,value):
+    def value(self, value: Tuple[int | float, str] | int | float):
+        self.value_setter(value=value)
+        for progress in self.regist_progress:
+            progress.progress(self._value, text=self.text)
+
+    
+    def value_setter(self,value: Tuple[int | float, str] | int | float):
+        """
+        方案1：(15,"等待中...")  同时放入数字和文字
+        方案2：15 只放入数字
+        """
+        assert isinstance(value,(tuple,int,float)),"值类型错误，只可以是tuple，int或者float中的一种"
+        if isinstance(value,tuple):
+            self.validate_value(value=value[0])
+            self._value = value[0]
+            self.text = value[1]
+        else:
+            self._value = value
+            self.text = ""
+
+    def regist_progress(self, obj):
+        self.regist.append(obj)
+
+    def __str__(self):
+        return self.text
+
+    def rerun(self):
+        """重运行时清理进度条注册表"""
+        self.regist.clear()
+
+
+class WatchDogItem(AbstractItem):
+    """监视文件数据"""
+
+class OnceItem(AbstractItem):
+    """只能访问一次就会变False的布尔值"""
+    def __init__(self, value:bool):
+        super().__init__(value)
+
+    @property
+    @abstractmethod
+    def value(self):
+        if self._value:
+            self._value = False
+            return True
+        return False
+
+    @value.setter
+    @abstractmethod
+    def value(self,value): 
         self._value = value
     
-    def set_progress_value(self):
-        """"""
-        page_id = st.session_state.now_page_id
-        obj = st.session_state[page_id]["data"]
-
-
+    def __bool__(self):
+        return self._value
+    
 if __name__ == "__main__":
     ai = AIHistory([124])
